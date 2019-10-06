cf5bfbbd8eea233c5b149ffb867bafad97b50745
Raslav Milutinovic <admin@rasovica.com>
2019-10-06 03:21:05 +0200

    feat: Add cms, typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,8 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-netlify-cms`,
+  ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const Index: React.FunctionComponent = () => {
+  return (
+    <div>Hello world!</div>
+  );
+}
+
+export default Index;
+
